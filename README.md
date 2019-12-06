18ae77cb9e019b7b216ef10025a6eca4f150f312
fumihumi <t.fum1hum@gmail.com>
2019-12-06 16:10:31 +0900

    init!

---
+++ README.md
@@ -0,0 +1,3 @@
+# ts-react-parcel-template
+
+this repository is inspired by [awesome-parcel/React with TypeScript](https://github.com/adhrinae/ts-react-parcel)
---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const App: React.FC = () => {
+  return (
+    <div>
+      <h3>This is App Component !!!!</h3>
+    </div>
+  );
+};
